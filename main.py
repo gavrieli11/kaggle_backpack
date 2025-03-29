d747401d939a8600373e477d5741f40431d2cc77
gavriely1 <daniel.gavriely@gmail.com>
2025-03-29 15:27:09 +0300

    first kaggle sunbmission

--- main.py
+++ main.py
@@ -6,72 +6,85 @@ from sklearn.metrics import mean_squared_error
 import torch.nn as nn
 import torch
 import torch.optim as optim
-df = pd.read_csv('data/train.csv')
+# df = pd.read_csv('data/train.csv')
 
-metadata = {}
-metadata['info']=df.info()
-metadata['n_unique']=df.nunique()
+# metadata = {}
+# metadata['info']=df.info()
+# metadata['n_unique']=df.nunique()
 
-# print(pd.DataFrame(metadata))
+# # print(pd.DataFrame(metadata))
 
-# print(df['Weight Capacity (kg)'])
+# # print(df['Weight Capacity (kg)'])
 
-df = df.rename(columns={'Weight Capacity (kg)': 'weight'})
-df = df.rename(columns={'Laptop Compartment': 'lap_comp'})
-df['lap_comp'] = df['lap_comp'].map({'Yes':1}).fillna(0)
-df['weight'] = df['weight'].fillna(df['weight'].mean())
+# df = df.rename(columns={'Weight Capacity (kg)': 'weight'})
+# df = df.rename(columns={'Laptop Compartment': 'lap_comp'})
+# df['lap_comp'] = df['lap_comp'].map({'Yes':1}).fillna(0)
+# df['weight'] = df['weight'].fillna(df['weight'].mean())
 
-num_vars = ['lap_comp', 'weight']
-X = df[num_vars]
-y = df['Price']
-lr = LinearRegression()
-lr.fit(X, y)
+# num_vars = ['lap_comp', 'weight']
+# X = df[num_vars]
+# y = df['Price']
+# lr = LinearRegression()
+# lr.fit(X, y)
 
-pred = lr.predict(X)
+# pred = lr.predict(X)
 
-print(mean_squared_error(y, pred))
+# print(mean_squared_error(y, pred))
 
-class mynn(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.input_layer = nn.Linear(2, 2)
-        self.active_func_layer_1 = nn.ReLU()
-        self.output_layer = nn.Linear(2, 1)
+# class mynn(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.input_layer = nn.Linear(2, 2)
+#         self.active_func_layer_1 = nn.ReLU()
+#         self.output_layer = nn.Linear(2, 1)
 
-    def forward(self, x):
-        x = self.input_layer(x)
-        x = self.active_func_layer_1(x)
-        x = self.output_layer(x)
-        return x
+#     def forward(self, x):
+#         x = self.input_layer(x)
+#         x = self.active_func_layer_1(x)
+#         x = self.output_layer(x)
+#         return x
     
-# tensors
-xt = torch.tensor(X.values, dtype=torch.float).to('cpu')
-yt = torch.tensor(y.values, dtype=torch.float).to('cpu')
-xt.requires_grad = True
-yt.requires_grad = True
+# # tensors
+# xt = torch.tensor(X.values, dtype=torch.float).to('cpu')
+# yt = torch.tensor(y.values, dtype=torch.float).to('cpu')
+# xt.requires_grad = True
+# yt.requires_grad = True
 
 
-# yt = yt.view(-1, 1)
-# model
-my_nn = mynn()
-optimizer = optim.Adam(my_nn.parameters(), lr=0.01)
-output = my_nn(xt).squeeze()
+# # yt = yt.view(-1, 1)
+# # model
+# my_nn = mynn()
+# optimizer = optim.Adam(my_nn.parameters(), lr=0.01)
+# output = my_nn(xt).squeeze()
 
-# forward pass
-loss_fn = nn.MSELoss()
-loss = loss_fn(output, yt)
-# backward pass
-loss.backward()
-print(loss)
-optimizer.step()
+# # forward pass
+# loss_fn = nn.MSELoss()
+# loss = loss_fn(output, yt)
+# # backward pass
+# loss.backward()
+# print(loss)
+# optimizer.step()
 
-# forward pass
-output = my_nn(xt).squeeze()
-loss = loss_fn(output, yt)
+# # forward pass
+# output = my_nn(xt).squeeze()
+# loss = loss_fn(output, yt)
 
-loss.backward()
-print(loss)
+# loss.backward()
+# print(loss)
 
+
+test = pd.read_csv("data/test.csv")
+# print(test)
+test['Price'] = 10.0
+# print(test['Brand'].mode())
+test['Brand'] = test['Brand'].fillna('mode')
+test['Material'] = test['Material'].fillna('mode')
+test['Size'] = test['Size'].fillna('mean')
+test['Laptop Compartment'] = test['Laptop Compartment'].fillna('mode')
+test = test.fillna('mode', axis=0)
+test[['id', 'Price']].to_csv('test_to_submit.csv', index=False, encoding='UTF-8')
+
+# print(test.isna().mean())
 
 
 
