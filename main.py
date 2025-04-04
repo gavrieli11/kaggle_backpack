dc75122db4d16297ca13d88ac050597ecfbb1a39
gavriely1 <daniel.gavriely@gmail.com>
2025-04-04 15:27:28 +0300

    second kaggle submit

--- main.py
+++ main.py
@@ -6,24 +6,29 @@ from sklearn.metrics import mean_squared_error
 import torch.nn as nn
 import torch
 import torch.optim as optim
-# df = pd.read_csv('data/train.csv')
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, Dataset
 
-# metadata = {}
-# metadata['info']=df.info()
-# metadata['n_unique']=df.nunique()
+df = pd.read_csv('data/train.csv')
 
-# # print(pd.DataFrame(metadata))
+metadata = {}
+metadata['info']=df.info()
+metadata['n_unique']=df.nunique()
 
-# # print(df['Weight Capacity (kg)'])
+# print(pd.DataFrame(metadata))
 
-# df = df.rename(columns={'Weight Capacity (kg)': 'weight'})
-# df = df.rename(columns={'Laptop Compartment': 'lap_comp'})
-# df['lap_comp'] = df['lap_comp'].map({'Yes':1}).fillna(0)
-# df['weight'] = df['weight'].fillna(df['weight'].mean())
+# print(df['Weight Capacity (kg)'])
 
-# num_vars = ['lap_comp', 'weight']
-# X = df[num_vars]
-# y = df['Price']
+df = df.rename(columns={'Weight Capacity (kg)': 'weight'})
+df = df.rename(columns={'Laptop Compartment': 'lap_comp'})
+df['lap_comp'] = df['lap_comp'].map({'Yes':1}).fillna(0)
+df['weight'] = df['weight'].fillna(df['weight'].mean())
+
+df, val = train_test_split(df, test_size=0.3, random_state=42)
+
+num_vars = ['lap_comp', 'weight']
+X = df[num_vars]
+y = df['Price']
 # lr = LinearRegression()
 # lr.fit(X, y)
 
@@ -31,58 +36,86 @@ import torch.optim as optim
 
 # print(mean_squared_error(y, pred))
 
-# class mynn(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.input_layer = nn.Linear(2, 2)
-#         self.active_func_layer_1 = nn.ReLU()
-#         self.output_layer = nn.Linear(2, 1)
+class mynn(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.input_layer = nn.Linear(2, 4)
+        self.active_func_layer_1 = nn.ReLU()
+        self.output_layer = nn.Linear(4, 1)
 
-#     def forward(self, x):
-#         x = self.input_layer(x)
-#         x = self.active_func_layer_1(x)
-#         x = self.output_layer(x)
-#         return x
+    def forward(self, x):
+        x = self.input_layer(x)
+        x = self.active_func_layer_1(x)
+        x = self.output_layer(x)
+        return x
     
-# # tensors
-# xt = torch.tensor(X.values, dtype=torch.float).to('cpu')
-# yt = torch.tensor(y.values, dtype=torch.float).to('cpu')
-# xt.requires_grad = True
-# yt.requires_grad = True
+# tensors
+xt = torch.tensor(X.values, dtype=torch.float).to('cpu')
+yt = torch.tensor(y.values, dtype=torch.float).to('cpu')
+xt.requires_grad = True
+yt.requires_grad = True
 
 
-# # yt = yt.view(-1, 1)
-# # model
-# my_nn = mynn()
-# optimizer = optim.Adam(my_nn.parameters(), lr=0.01)
-# output = my_nn(xt).squeeze()
+# model
+my_nn = mynn().to('cpu')
+opt = optim.Adam(my_nn.parameters(), lr=0.01)
+output = my_nn(xt).squeeze()
+loss_vals = []
+epochs = 2
 
-# # forward pass
-# loss_fn = nn.MSELoss()
-# loss = loss_fn(output, yt)
-# # backward pass
-# loss.backward()
-# print(loss)
-# optimizer.step()
+# Data loader
+class MyDataset(Dataset):
+    def __init__(self, x, y):
+        super().__init__()
+        self.x = torch.tensor(x).float()
+        self.y = torch.tensor(y).float()
 
-# # forward pass
-# output = my_nn(xt).squeeze()
-# loss = loss_fn(output, yt)
+    def __len__(self):
+        return len(self.x)
+    
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
 
-# loss.backward()
-# print(loss)
+dataset = MyDataset(X.values, y.values)
+
+dl = DataLoader(dataset, batch_size=30, shuffle=True) # How to define data loader??
+
+for _ in range(epochs):
+    for data in dl:
+        x, y = data
+        opt.zero_grad()
+        loss_func = nn.MSELoss()
+        loss_val = loss_func(my_nn(x), y)
+        loss_val.backward()
+        opt.step()
+        loss_vals.append(loss_val.item())
 
 
 test = pd.read_csv("data/test.csv")
-# print(test)
-test['Price'] = 10.0
-# print(test['Brand'].mode())
-test['Brand'] = test['Brand'].fillna('mode')
-test['Material'] = test['Material'].fillna('mode')
-test['Size'] = test['Size'].fillna('mean')
-test['Laptop Compartment'] = test['Laptop Compartment'].fillna('mode')
-test = test.fillna('mode', axis=0)
-test[['id', 'Price']].to_csv('test_to_submit.csv', index=False, encoding='UTF-8')
+# # print(test)
+# test['Price'] = 10.0
+test_ids = test['id'].values
+test = test.rename(columns={'Weight Capacity (kg)': 'weight'})
+test = test.rename(columns={'Laptop Compartment': 'lap_comp'})
+test['lap_comp'] = df['lap_comp'].map({'Yes':1}).fillna(0)
+test['weight'] = test['weight'].fillna(test['weight'].mean())
+test = test[['lap_comp', 'weight']].values
+test = torch.tensor(test).float()
+pred = my_nn(test).detach()
+pred = pred.reshape(len(pred))
+test_out = pd.DataFrame({'id':test_ids ,'Price': pred})
+test_out['Price'] = test_out['Price'].fillna(np.mean(test_out['Price']))
+print(test_out)
+
+# test['Price'] = my_nn(test.values)
+# # print(test['Brand'].mode())
+# test['Brand'] = test['Brand'].fillna('mode')
+# test['Material'] = test['Material'].fillna('mode')
+# test['Size'] = test['Size'].fillna('mean')
+# test['Laptop Compartment'] = test['Laptop Compartment'].fillna('mode')
+# test = test.fillna('mode', axis=0)
+# test[['id', 'Price']].to_csv('test_to_submit.csv', index=False, encoding='UTF-8')
+test_out.to_csv('test_to_submit_2.csv', index=False, encoding='UTF-8')
 
 # print(test.isna().mean())
 
---
+++ temp.py
@@ -0,0 +1,6 @@
+import torch
+
+# print(torch.randn(2, 4, 3))
+# print(torch.randn(2, 4, 3).shape)
+# print(torch.randn(2, 4, 3).mean(axis=1))
+print(torch.randn(2, 4, 3))
